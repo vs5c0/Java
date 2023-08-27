0e45ff4238f8c9969e22fe08884a4736b27966ae
sai <127922969+vs5c0@users.noreply.github.com>
2023-08-27 22:29:42 +0530

    Add files via upload

---
+++ Day5/form.java
@@ -0,0 +1,14 @@
+
+
+class form
+{
+ public static void main(String[] args) {
+    int i=1;
+    for( i=1;i<=5;i++)
+    {
+        System.out.println("Chandrayyan 3 is give a great Boost to India");
+    }
+
+    
+ }
+}
