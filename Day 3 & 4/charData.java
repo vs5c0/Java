859515a273de31794f8442c69b764c6c981be3d2
sai <127922969+vs5c0@users.noreply.github.com>
2023-09-03 18:23:41 +0530

    Delete Day 3 & 4 directory

--- Day 3 & 4/charData.java
+++
@@ -1,9 +0,0 @@
-class charData
-{
-    public static void main(String[] args)
-    {
-        //ocupies only 2 bytes of memory
-        char gender = 'M';
-        System.out.println("Gender :"+gender);
-    }
-}
