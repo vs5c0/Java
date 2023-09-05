14c44d8c5f60b7555624040d8eaf315e907f1273
sai <127922969+vs5c0@users.noreply.github.com>
2023-09-05 14:50:47 +0530

    Add files via upload

---
+++ Basic Java/ConditionalStmts/ifStmt.java
@@ -0,0 +1,15 @@
+package ConditionalStmts;
+
+public class ifStmt {
+    
+    public static void main(String[] args) {
+        
+         int a = 10;
+         if(a>0)
+
+         
+          System.out.println( a+ " a is Postive number");
+         
+
+    }
+}
---
+++ Basic Java/ConditionalStmts/ifelseStmt.java
@@ -0,0 +1,19 @@
+package ConditionalStmts;
+
+ class ifelseStmt {
+    public static void main(String[] args) {
+        
+        int a = 40;
+        if(a%2==0)
+        {
+            System.out.println("a is Even");
+            System.out.println("Hello AP");
+        }
+        else
+        {
+            System.out.println("a in Not a Even");
+            System.out.println("Hello GP");
+        }
+    }
+    
+}
