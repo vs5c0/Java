3a23828a840b53e6597d3dcb2725863ef6136ae2
sai <127922969+vs5c0@users.noreply.github.com>
2023-09-13 22:48:34 +0530

    Add files via upload

---
+++ Basic Java/Methods/Demo.java
@@ -0,0 +1,15 @@
+package Methods;
+
+  public class Demo {
+    public static void main(String[] args) {
+         
+        Cal c = new Cal();
+        c.add(12,12);
+    }
+  }
+class Cal{
+    public int add(int n1, int n2){
+        return n1+n2;
+    }
+}
+  
