5f4e665a724274717c15d1bb9833af337940e9c1
sai <127922969+vs5c0@users.noreply.github.com>
2023-09-15 21:07:34 +0530

    Add files via upload

---
+++ Basic Java/Arrays/Arr.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+ class Arr {
+
+    public static void main(String[] args) {
+         //One Form of Array Creation
+        int ar[] = {32,2,3,2,1};
+        System.out.println(ar[1]);
+    
+        //Another Form of Array Creation
+    
+    int n[] = new int[5];
+    n[0]=12;
+    n[1]=2;
+    n[2]=5;
+    n[3]=7;
+    System.out.println(n[0]);
+    System.out.println(n[1]);
+    System.out.println(n[2]);
+    System.out.println(n[3]);
+
+
+    int na[] = new int[5];
+    na[0]=121;
+    na[1]=21;
+    na[2]=35;
+    na[3]=74;
+    for(int i=0;i<4;i++){
+    System.out.println(na[i]);
+
+    }
+} 
+
+ }
