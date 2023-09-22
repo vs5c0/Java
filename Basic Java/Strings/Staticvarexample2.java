8f2915414e058534ced287376e1eb085dd297d87
sai <127922969+vs5c0@users.noreply.github.com>
2023-09-22 22:19:34 +0530

    Add files via upload

---
+++ Basic Java/Strings/Staticvarexample2.java
@@ -0,0 +1,16 @@
+package Strings;
+
+ class Staticvarexample2 {
+
+    private static  double salary;
+
+    public static final String Department = "Development";
+    
+    public static void main(String[] args) {
+        
+        salary = 10000;
+
+        System.out.println(Department +"Avg Salary:"+ salary);
+    }
+    
+}
