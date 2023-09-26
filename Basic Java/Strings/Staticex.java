f85e02615a6e7e4ea18746cbb16f9e866b3ab40b
sai <127922969+vs5c0@users.noreply.github.com>
2023-09-26 21:53:11 +0530

    Add files via upload

---
+++ Basic Java/Strings/Staticex.java
@@ -0,0 +1,14 @@
+package Strings.StrandMethods;
+
+public class Staticex {
+    static void m1(){
+        System.out.println("From M1");
+
+        
+    }
+    public static void main(String[] args) {
+        
+        m1();
+    }
+    
+}
