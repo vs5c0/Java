8d87e4713ec060f32f6a2d744b7a7dac53ec3ddb
sai <127922969+vs5c0@users.noreply.github.com>
2023-10-05 21:02:59 +0530

    Add files via upload

---
+++ Basic Java/AdvanceJava/Constructor.java
@@ -0,0 +1,19 @@
+package AdvanceJava;
+
+import java.util.*;
+
+class Constructor
+{
+ /*
+  * Constructor ---> Special method in java 
+  * block of Code Similar to Method .cant return anything , constrcutor can be created by using class Name
+  */
+    public Constructor() 
+    {
+        System.out.println("It is a Constructor ");
+    }
+
+    public static void main(String[] args) {
+        Constructor obj = new Constructor();
+    }
+}
