be96df55b159a60f7bd854b83929c247e7780052
sai <127922969+vs5c0@users.noreply.github.com>
2023-10-05 21:03:19 +0530

    Add files via upload

---
+++ Basic Java/AdvanceJava/DefaultConstructor.java
@@ -0,0 +1,17 @@
+package AdvanceJava;
+
+class DefaultConstructor {
+
+    // Default Constructor ---> can't have any Parameters 
+
+    DefaultConstructor()
+    {
+        System.out.println("It is Default Constructor");
+    }
+
+    public static void main(String[] args) {
+        
+        DefaultConstructor obj = new DefaultConstructor();
+    }
+    
+}
