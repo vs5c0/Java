257538955d32e2c1ff0f3a09cd8c5adb8d87e871
sai <127922969+vs5c0@users.noreply.github.com>
2023-10-07 09:38:11 +0530

    Add files via upload

---
+++ Basic Java/AdvanceJava/Anonymous.java
@@ -0,0 +1,32 @@
+package AdvanceJava;
+
+class Anonymous {
+
+    int a;
+    int b;
+    int c;
+    int d;
+
+    public Anonymous(int x, int y)
+    {
+        a=x;
+        b=y;
+        int ab = x+y;
+        System.out.println("Addition of two numbers : " +ab);
+    }
+    
+    public void Sub(int m,int n)
+   {
+         c=m;
+         d=n;
+         int cd=m-n;
+         System.out.println("Sub of Two Number :" +cd);
+
+   } 
+
+   public static void main(String[] args) {
+    
+    new Anonymous(12,2).Sub(12,10); //Anonoymous Object --> dont have any Name
+     new Anonymous(10,12).Sub(100,10);
+   }
+}
