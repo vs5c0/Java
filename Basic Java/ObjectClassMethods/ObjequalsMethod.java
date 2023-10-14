fab7d2250c5a91acfaa4dfb6b642e40282d8bc84
sai <127922969+vs5c0@users.noreply.github.com>
2023-10-14 14:20:25 +0530

    Add files via upload

---
+++ Basic Java/ObjectClassMethods/ObjequalsMethod.java
@@ -0,0 +1,25 @@
+package ObjectClassMethods;
+
+
+
+// equal() Method ---> used to compare two strings based on the content of string
+ class ObjequalsMethod {
+
+    public static void main(String[] args) {
+        
+        String s1 = "JavaEasy";
+        String s2 =  "JavaEasy";
+        System.out.println(s1.equals(s2));
+        if(s1.equals(s2))
+        {
+            System.out.println("True Stmts");
+        }
+        else
+        {
+            System.out.println("False Stmts");
+        }
+    }
+
+
+    
+}
